388c5acf376f09c91731ab2fbf8ac88fbfc866ab
Link4Electronics <link4electronics@gmail.com>
2026-08-30 05:33:26 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -17,3 +17,7 @@ mv -v /opt/dwarffortress/data ./AppDir/bin
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
+
+# Test the app for 12 seconds, if the app normally quits before that time
+# then skip this or check if some flag can be passed that makes it stay open
+quick-sharun --simple-test ./dist/*.AppImage
